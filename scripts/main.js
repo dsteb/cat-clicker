d662f796e9324202d5f5686a1c44b55f8ac7e4e7
Dmitrii Stebliuk <dsteblyuk@gmail.com>
2016-08-02 19:13:57 +0200

    feat: Add utility String.format function
    String.format is the only function modifying default js objects prototyps.

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,15 @@ var $ = document.querySelector.bind(document);
 (function() {
   'use strict';
 
+  if (!String.prototype.format) {
+    String.prototype.format = function() {
+      var args = arguments;
+      return this.replace(/{(\d+)}/, function(m, i) {
+        return args[i] ? args[i] : m;
+      });
+    };
+  }
+
   var gioaCounter = 0;
   $('#gioa-img').addEventListener('click', function() {
     $('#gioa-counter').innerText = ++gioaCounter;
