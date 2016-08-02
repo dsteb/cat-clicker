a77ac44437a4c06d30c1abb6d09c43e7c3dc0ca6
Dmitrii Stebliuk <dsteblyuk@gmail.com>
2016-08-02 19:29:58 +0200

    refact: Refactoring 2 cats clicker
    Removing code duplication and hardcoding of cat names and blocks.

--- scripts/main.js
+++ scripts/main.js
@@ -12,14 +12,54 @@ var $ = document.querySelector.bind(document);
     };
   }
 
-  var gioaCounter = 0;
-  $('#gioa-img').addEventListener('click', function() {
-    $('#gioa-counter').innerText = ++gioaCounter;
-  });
+  var Cat = function(name) {
+    this.name = name;
+    this.img = 'images/{0}.jpg'.format(name.toLowerCase());
 
-  var brunoCounter = 0;
-  $('#bruno-img').addEventListener('click', function() {
-    $('#bruno-counter').innerText = ++brunoCounter;
-  });
+    var container = document.createElement('div');
+    container.className += 'cat';
+
+    var headerEl = document.createElement('h2');
+    var header = 'Click {0}!'.format(name);
+    headerEl.innerText = header;
+
+    var img = document.createElement('img');
+    img.className += 'img-responsive clickable';
+    img.id = name.toLowerCase() + '-img';
+    img.src = this.img;
+    img.alt = 'Kitty ' + name;
+    img.title = 'Click me please!';
+
+    var counterBlock = document.createElement('div');
+    counterBlock.innerText = '{0} clicker: '.format(name);
+
+    var counter = 0;
+    var counterEl = document.createElement('em');
+    counterEl.innerText = counter;
+
+    img.addEventListener('click', function() {
+      counterEl.innerText = ++counter;
+    });
+
+    counterBlock.appendChild(counterEl);
+
+    container.appendChild(headerEl);
+    container.appendChild(img);
+    container.appendChild(counterBlock);
+
+    $('#cats').appendChild(container);
+  };
+
+  new Cat('Gioa');
+  new Cat('Bruno');
+  // var gioaCounter = 0;
+  // $('#gioa-img').addEventListener('click', function() {
+  //   $('#gioa-counter').innerText = ++gioaCounter;
+  // });
+  //
+  // var brunoCounter = 0;
+  // $('#bruno-img').addEventListener('click', function() {
+  //   $('#bruno-counter').innerText = ++brunoCounter;
+  // });
 
 })();
