5440da6c22581e790efd79acd31e8626278f32d5
Dmitrii Stebliuk <dsteblyuk@gmail.com>
2016-08-10 18:47:51 +0200

    refact: Doing the same with knockout
    The feature list is cut, now you have only 1 cat.

--- scripts/main.js
+++ scripts/main.js
@@ -15,132 +15,16 @@
     };
   }
 
-  var model = {
-    currentCat: null,
-    cats: [
-      {
-        name: 'Васька',
-        image: 'images/kitty.jpg',
-        counter: 0
-      }, {
-        name: 'Бруно',
-        image: 'images/hidden-cat.jpg',
-        counter: 0
-      }, {
-        name: 'Тася',
-        image: 'images/tay-cat.jpg',
-        counter: 0
-      }, {
-        name: 'Тося',
-        image: 'images/bold-cat.jpg',
-        counter: 0
-      }, {
-        name: 'Жужу',
-        image: 'images/black-white.jpg',
-        counter: 0
-      }],
+  var ViewModel = function() {
+    this.cat = {
+      name: 'ЖуЖу',
+      imageSrc: 'images/black-white.jpg',
+      counter: ko.observable(0)
+    };
+    this.incrementClickCounter = function() {
+      this.cat.counter(this.cat.counter() + 1);
+    };
   };
 
-  var octopus = {
-    init: function() {
-      catListView.init();
-      $('#cat-image').addEventListener('click', octopus.increaseCounter);
-      model.currentCat = model.cats[0];
-      catView.render(model.currentCat);
-      adminView.render(model.currentCat);
-      var target = $('#cat-list > li:first-child > a');
-      catListView.select(target);
-      adminView.init();
-    },
-    getCats: function() {
-      return model.cats;
-    },
-    increaseCounter: function() {
-      var currentCat = model.currentCat;
-      if (currentCat) {
-        ++currentCat.counter;
-        catView.render(currentCat);
-        adminView.render(currentCat);
-      }
-    },
-    setCurrentCat: function(cat) {
-      if (model.currentCat === cat) return false;
-      model.currentCat = cat;
-      return true;
-    },
-    updateCurrentCat: function(newName, newImage, newCounter) {
-      var cat = model.currentCat;
-      cat.name = newName;
-      cat.image = newImage;
-      cat.counter = newCounter;
-      catView.render(cat);
-    },
-    closeAdmin: function() {
-      adminView.hide();
-      adminView.render(model.currentCat);
-    }
-  };
-
-  var catListView = {
-    init: function() {
-      var cats = octopus.getCats();
-      cats.forEach(function(cat) {
-        var li = document.createElement('li');
-        var link = document.createElement('a');
-        link.href = '#';
-        link.textContent = cat.name ;
-        link.addEventListener('click', (function(cat) {
-          return function(e) {
-            if (octopus.setCurrentCat(cat)) {
-              catView.render(cat);
-              catListView.select(e.target);
-              adminView.render(cat);
-            }
-          };
-        })(cat));
-        li.appendChild(link);
-        $('#cat-list').appendChild(li);
-      });
-    },
-    select: function(a) {
-      $('.selected').className = '';
-      a.className = 'selected';
-    }
-  };
-
-  var catView = {
-    render: function(cat) {
-      $('.cat-name').forEach(function(el) {
-        el.textContent = cat.name;
-      });
-      $('#cat-counter').textContent = cat.counter;
-      $('#cat-image').src = cat.image;
-    }
-  };
-
-  var adminView = {
-    init: function() {
-      $('#admin-btn').addEventListener('click', adminView.show);
-      $('#admin-save-btn').addEventListener('click', function() {
-        var newName = $('#cat-name-input').value;
-        var newImage = $('#cat-image-input').value;
-        var newCounter = parseInt($('#cat-counter-input').value);
-        octopus.updateCurrentCat(newName, newImage, newCounter);
-      });
-      $('#admin-cancel-btn').addEventListener('click', octopus.closeAdmin);
-    },
-    show: function() {
-      $('#admin-section').style = '';
-    },
-    hide: function() {
-      $('#admin-section').style = 'display: none;';
-    },
-    render: function(cat) {
-      $('#cat-name-input').value = cat.name;
-      $('#cat-image-input').value = cat.image;
-      $('#cat-counter-input').value = cat.counter;
-    }
-  };
-
-  octopus.init();
+  ko.applyBindings(new ViewModel());
 })();
